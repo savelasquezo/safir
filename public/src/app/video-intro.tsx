49d6640d2fe0257169c0216ea083a787d2c138f6
Alex <dalex743@gmail.com>
2024-06-09 18:15:01 -0500

    Modificada reproducción de video

--- public/src/app/video-intro.tsx
+++ public/src/app/video-intro.tsx
@@ -7,7 +7,6 @@ export function VideoIntro() {
   return (
     <div className="p-8">
       <div className="w-full container px-5 overflow-hidden rounded-xl relative mx-auto mt-10 max-w-6xl ">
-        <div className="z-10 absolute w-full h-full inset-0 rounded-xl" />
         <video className="h-full w-full rounded-lg" controls muted>
           <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
           Your browser does not support the video tag.

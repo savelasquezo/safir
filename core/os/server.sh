4472a5e00cdf41ecbdd7dc1c1fa0d3d480611b42
savelasquezo <savelasquezo@gmail.com>
2024-06-09 17:38:17 -0500

    Rebase Alex

--- core/os/server.sh
+++ core/os/server.sh
@@ -2,6 +2,6 @@
 DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
 DJANGO_SETTINGS_MODULE=core.settings
 cd $DJANGODIR
-source /app/safir/core/venv/bin/activate
+source /app/asesoriasafir/core/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 exec python3 manage.py runserver 213.218.240.170:8000

b1d64fb80979be88e47a0ae217d34d600e47e48e
root <root@srv535980.hstgr.cloud>
2024-06-09 02:33:17 +0000

    Ubuntu Deployment

---
+++ core/os/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
+DJANGO_SETTINGS_MODULE=core.settings
+cd $DJANGODIR
+source /app/safir/core/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+exec python3 manage.py runserver 213.218.240.170:8000

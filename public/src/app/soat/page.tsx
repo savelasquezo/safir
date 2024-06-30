515ac33275b7bcb488b5e0f988b9cbbc252aa437
savelasquezo <savelasquezo@gmail.com>
2024-06-30 18:04:00 -0500

    NewChanges /Admin Video-Features

--- public/src/app/soat/page.tsx
+++ public/src/app/soat/page.tsx
@@ -1,13 +1,30 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 import { SessionProvider } from 'next-auth/react';
 import { Navbar, Footer } from "@/components";
-
-// sections
 import ProductosSoat from "./productos-soat";
 
 export default function Home() {
+
+  useEffect(() => {
+    const fetchSettings = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_APP_API_URL}/app/v1/manager/fetch-settings/`);
+        const settings = response.data;
+        Object.entries(settings).forEach(([key, value]) => {
+          localStorage.setItem(`set_${key}`, JSON.stringify(value));
+        });
+      } catch (error) {
+        console.error('Error fetching settings:', error);
+      }
+    };
+
+    fetchSettings();
+  },);
+
   return (
     <SessionProvider>
       <>
